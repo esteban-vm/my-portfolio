dd813a6efe758e26041853b3f461f7a199d80425
esteban-vm <estebanvm90@outlook.com>
2024-06-03 09:09:07 -0400

    refactors

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,5 +1,3 @@
-'use client'
-
 export { default as CuteHomeRobot } from './cute_home_robot'
 export { default as CyberpunkCar } from './cyberpunk_car'
 export { default as CyberpunkCity } from './cyberpunk_city'
