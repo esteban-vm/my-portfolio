449d6eff6715bc4fa1ea927b6a166e95448aeec7
esteban-vm <estebanvm90@outlook.com>
2024-06-08 15:43:01 -0400

    fix navbar style

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -6,7 +6,7 @@ export default function Navigation() {
   return (
     <header className='container pointer-events-none absolute left-1/2 top-16 z-10 flex -translate-x-1/2 select-none items-center justify-around font-montserrat uppercase text-white pointer-coarse:portrait:top-11 pointer-coarse:landscape:top-8 [&_a]:pointer-events-auto [&_button]:pointer-events-auto'>
       <Link
-        className='z-20 animate-glitch text-center font-saiba45 text-5xl text-cyber logo-shadow hover:animate-none md:text-6xl lg:text-7xl'
+        className='animate-glitch text-center font-saiba45 text-5xl text-cyber logo-shadow hover:animate-none md:text-6xl lg:text-7xl'
         href='/'
       >
         Esteban
