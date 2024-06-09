18c892135f9d60d890440b7a6e2f509d4fb7b623
esteban-vm <estebanvm90@outlook.com>
2024-06-09 17:28:16 -0400

    refactor navigation bar

---
+++ src/components/nav_bar/app_logo.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+
+export default function AppLogo() {
+  return (
+    <Link
+      className='animate-glitch text-center font-saiba45 text-5xl text-cyber logo-shadow hover:animate-none md:text-6xl lg:text-7xl'
+      href='/'
+    >
+      Esteban
+      <span className='hidden md:inline'>&nbsp;</span>
+      <br className='md:hidden' />
+      V.M.
+    </Link>
+  )
+}
