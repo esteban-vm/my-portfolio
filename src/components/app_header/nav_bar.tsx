52b67b71aad95c7d188efccb0bc08009fa06b06d
esteban-vm <estebanvm90@outlook.com>
2024-06-17 13:42:03 -0400

    fix navbar styles

--- src/components/app_header/nav_bar.tsx
+++ src/components/app_header/nav_bar.tsx
@@ -7,13 +7,13 @@ import { LuMenu, LuX } from 'react-icons/lu'
 import { clsx } from '@/utils'
 
 export default function NavBar({ children }: { children: ReactNode }) {
-  const [active, setActive] = useState(false)
-  const isMediumDevice = useMediaQuery({ query: '(max-width: 1023px), (pointer: coarse)' })
-  const toggleMenu = () => setActive(!active)
+  const [isActive, setIsActive] = useState(false)
+  const isMediumDevice = useMediaQuery({ query: '(max-width: 1023px)' })
+  const toggleMenu = () => setIsActive(!isActive)
 
   useEffect(() => {
     if (isMediumDevice) {
-      const closeMenu = () => setActive(false)
+      const closeMenu = () => setIsActive(false)
       const links = document.querySelectorAll('a[data-navigation]')
       links.forEach((link) => link.addEventListener('click', closeMenu))
       return () => links.forEach((link) => link.removeEventListener('click', closeMenu))
@@ -26,12 +26,12 @@ export default function NavBar({ children }: { children: ReactNode }) {
         className='pointer-events-auto flex items-center justify-center text-neon-yellow lg:hidden [&>svg]:size-8 active:[&>svg]:scale-90 md:[&>svg]:size-10'
         onClick={toggleMenu}
       >
-        {active ? <LuX /> : <LuMenu />}
+        {isActive ? <LuX /> : <LuMenu />}
       </button>
       <nav
         className={clsx(
-          'absolute right-0 z-10 flex w-full flex-col items-center justify-around text-nowrap bg-black/50 text-center animate__animated nav-h md:nav-h-md lg:static lg:size-auto lg:animate-none lg:flex-row lg:gap-8 lg:bg-transparent',
-          active ? 'top-[150%] animate__backInDown' : 'top-0 animate__backOutUp'
+          'absolute right-0 flex w-full flex-col items-center justify-around text-nowrap text-center animate__animated nav-h md:nav-h-md lg:static lg:size-auto lg:animate-none lg:flex-row lg:gap-8',
+          isActive ? 'top-[150%] bg-black/50 animate__backInDown' : 'animate__backOutUp'
         )}
       >
         {children}
