5d1985ac8aa0b7231a28ab84df82853ed8c7233c
esteban-vm <estebanvm90@outlook.com>
2024-06-20 22:53:06 -0400

    refactors

--- src/components/app_header/nav_link.tsx
+++ src/components/app_header/nav_link.tsx
@@ -1,23 +1,22 @@
 'use client'
 
-import type { NavLink as INavLink } from '@/types'
+import type { LinkProps } from 'next/link'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { clsx } from '@/utils'
 
-export default function NavLink({ title, route }: INavLink) {
+export default function NavLink<T extends string>({ href, ...rest }: LinkProps<T>) {
   const pathname = usePathname()
 
   return (
     <Link
-      href={route}
+      {...rest}
+      href={href}
       className={clsx(
         'relative inline-block font-bold uppercase text-neon-yellow after:bottom-0 after:left-0 after:h-1 after:w-full after:origin-bottom-right after:scale-x-0 after:bg-neon-yellow after:transition-transform after:duration-200 after:ease-out after:content-[""] hover:opacity-90 hover:after:origin-bottom-left hover:after:scale-x-100 active:scale-95 lg:p-0 lg:after:absolute',
-        pathname === route && '!text-neon-green-dark after:!bg-neon-green-dark'
+        pathname === href && '!text-neon-green-dark after:!bg-neon-green-dark'
       )}
       data-navigation
-    >
-      {title}
-    </Link>
+    />
   )
 }
--- src/constants/index.ts
+++
@@ -1,7 +0,0 @@
-import type { NavLink } from '@/types'
-
-export const navLinks: NavLink[] = [
-  { title: 'About Me', route: '/about' },
-  { title: 'My Projects', route: '/projects' },
-  { title: 'Contact Me', route: '/contact' },
-]
