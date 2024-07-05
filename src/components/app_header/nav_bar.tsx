840b68fe786527be02519031633291cc00336720
esteban-vm <estebanvm90@outlook.com>
2024-07-05 16:59:23 -0400

    fix navbar styles

--- src/components/app_header/nav_bar.tsx
+++ src/components/app_header/nav_bar.tsx
@@ -7,7 +7,7 @@ interface NavBarProps extends HTMLAttributes<HTMLElement> {
 
 export default function NavBar({ isOpen, ...rest }: NavBarProps) {
   const classes = clsx(
-    'absolute flex w-[95%] flex-col items-center justify-around text-nowrap rounded-lg',
+    'absolute flex w-[95%] flex-col items-center justify-around text-nowrap',
     'bg-black/60 text-center duration-700 fill-mode-forwards nav-h md:nav-h-md',
     'lg:static lg:size-auto lg:animate-none lg:flex-row lg:gap-8 lg:bg-transparent',
     isOpen ? 'top-[150%] animate-in slide-in-from-top' : 'top-[-500%] animate-out slide-out-to-top'
