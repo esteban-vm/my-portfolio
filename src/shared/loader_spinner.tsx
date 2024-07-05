90d9d9834a67b2b32a0bd93ccc01533afbecaf2b
esteban-vm <estebanvm90@outlook.com>
2024-07-05 16:28:35 -0400

    update loader style

--- src/shared/loader_spinner.tsx
+++ src/shared/loader_spinner.tsx
@@ -3,5 +3,5 @@ import { PacmanLoader } from 'react-spinners'
 type LoaderSpinnerProps = Parameters<typeof PacmanLoader>[0]
 
 export default function LoaderSpinner(props: LoaderSpinnerProps) {
-  return <PacmanLoader {...props} aria-label='Loading Spinner' color='fuchsia' loading />
+  return <PacmanLoader {...props} aria-label='Loading Spinner' color='#00e0c0' speedMultiplier={2} />
 }
