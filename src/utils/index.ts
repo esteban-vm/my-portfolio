b76035ac954519d0234191548b3877931a6cd413
esteban-vm <estebanvm90@outlook.com>
2024-07-15 13:23:55 -0400

    refactors

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,9 +2,9 @@ import type { ModelType } from '@/types'
 import type { ClassValue } from 'clsx'
 import { clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
-import { models } from '@/constants'
+import { Models } from '@/constants'
 
 const twClsx = (...inputs: ClassValue[]) => twMerge(clsx(inputs))
-const getModelPath = <T extends ModelType>(model: T) => <const>`/models/${models[model]}.glb`
+const getModelPath = <T extends ModelType>(model: T) => <const>`/models/${Models[model]}.glb`
 
 export { twClsx as clsx, getModelPath }
