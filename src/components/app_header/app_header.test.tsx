97097355bf7cc5162b952d2ea6cf55c448c11de0
esteban-vm <estebanvm90@outlook.com>
2024-07-19 12:00:33 -0400

    refine tests

--- src/components/app_header/app_header.test.tsx
+++ src/components/app_header/app_header.test.tsx
@@ -1,9 +1,10 @@
-import { render } from '@/tests'
+import { render, screen } from '@/tests'
 import AppHeader from './app_header'
 
 describe(`${AppHeader.name}:`, () => {
   it('should render correctly', () => {
     const { asFragment } = render(<AppHeader />)
     expect(asFragment()).toMatchSnapshot()
+    expect(screen.getByRole('banner')).toBeInTheDocument()
   })
 })
