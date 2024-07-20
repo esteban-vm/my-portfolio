29a2352bb15010d7678638a2537feff093aa00ae
esteban-vm <estebanvm90@outlook.com>
2024-07-20 11:05:20 -0400

    update scene context

--- src/contexts/scene_context.tsx
+++ src/contexts/scene_context.tsx
@@ -1,5 +1,5 @@
 import type { ReactNode } from 'react'
-import { createContext, useContext, useState } from 'react'
+import { createContext, useCallback, useContext, useMemo, useState } from 'react'
 import { useMediaQuery } from 'react-responsive'
 
 interface ISceneContext {
@@ -7,7 +7,8 @@ interface ISceneContext {
   currentStage: AppTypes.CurrentStage
   setCurrentStage: (currentStage: AppTypes.CurrentStage) => void
   isAnimated: boolean
-  setIsAnimated: (isAnimated: boolean) => void
+  rotateScene: () => void
+  stopScene: () => void
 }
 
 const SceneContext = createContext<ISceneContext>(null!)
@@ -17,13 +18,19 @@ export function SceneContextProvider(props: { children: ReactNode }) {
   const [currentStage, setCurrentStage] = useState<AppTypes.CurrentStage>(1)
   const [isAnimated, setIsAnimated] = useState(true)
 
-  const value: ISceneContext = {
-    isMobile,
-    currentStage,
-    setCurrentStage,
-    isAnimated,
-    setIsAnimated,
-  }
+  const rotateScene = useCallback(() => setIsAnimated(true), [])
+  const stopScene = useCallback(() => setIsAnimated(false), [])
+
+  const value = useMemo((): ISceneContext => {
+    return {
+      isMobile,
+      currentStage,
+      setCurrentStage,
+      isAnimated,
+      rotateScene,
+      stopScene,
+    }
+  }, [currentStage, isAnimated, isMobile, rotateScene, stopScene])
 
   return <SceneContext.Provider value={value} {...props} />
 }
