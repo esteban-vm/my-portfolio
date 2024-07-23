c738512025bb650f393ab23c48c5117e580c4cb0
esteban-vm <estebanvm90@outlook.com>
2024-07-23 12:12:57 -0400

    change to Suspense and lazy

--- src/models/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as CuteHomeRobot } from './cute_home_robot'
-export { default as CyberpunkCity } from './cyberpunk_city'
-export { default as CyberpunkGhettoDeLorean } from './cyberpunk_ghetto_delorean'
--- src/scenes/home_scene.tsx
+++ src/scenes/home_scene.tsx
@@ -1,44 +1,49 @@
-import { Environment, Html, OrbitControls, PerspectiveCamera } from '@react-three/drei'
+import { Environment, Html, OrbitControls, PerspectiveCamera, useProgress } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
-import dynamic from 'next/dynamic'
+import { Suspense, lazy } from 'react'
 import { useSceneContext } from '@/contexts'
 import { LoadingSpinner } from '@/shared'
 
-const CyberpunkCity = dynamic(() => import('@/models').then((mod) => mod.CyberpunkCity), {
-  loading() {
-    return (
-      <Html center>
-        <LoadingSpinner size={30} />
-      </Html>
-    )
-  },
-})
+const CyberpunkCity = lazy(() => import('@/cyberpunk_city'))
+const CyberpunkGhettoDeLorean = lazy(() => import('@/cyberpunk_ghetto_delorean'))
 
-const CyberpunkGhettoDeLorean = dynamic(() => import('@/models').then((mod) => mod.CyberpunkGhettoDeLorean))
+function Loader() {
+  const { progress } = useProgress()
+  const percent = (progress / 100).toLocaleString(undefined, { style: 'percent' })
+
+  return (
+    <Html className='text-center' center>
+      <span className='font-bold text-neon-green-light'>{percent}</span>
+      <LoadingSpinner className='-left-1/2' />
+    </Html>
+  )
+}
 
 export default function HomeScene() {
   const { isMobile, isAnimated } = useSceneContext()
 
   return (
     <Canvas className='cursor-grab active:cursor-grabbing'>
-      <CyberpunkCity
-        position={[-2, isMobile ? -2.5 : -2, 2]}
-        rotation={[0, Math.PI / 2, 0]}
-        scale={isMobile ? 1 : 1.2}
-      />
-      <CyberpunkGhettoDeLorean rotation={[0, Math.PI / 2, 0.2]} scale={isMobile ? 0.15 : 0.2} />
-      <directionalLight intensity={10} position={[0, 1, 0]} />
-      <Environment preset='night' />
-      <PerspectiveCamera position={[0, 0, 10]} makeDefault />
-      <OrbitControls
-        autoRotate={isAnimated}
-        autoRotateSpeed={-0.5}
-        enablePan={false}
-        enableZoom={false}
-        maxPolarAngle={Math.PI / 2}
-        minPolarAngle={Math.PI / 2}
-        enableDamping
-      />
+      <Suspense fallback={<Loader />}>
+        <CyberpunkCity
+          position={[-2, isMobile ? -2.5 : -2, 2]}
+          rotation={[0, Math.PI / 2, 0]}
+          scale={isMobile ? 1 : 1.2}
+        />
+        <CyberpunkGhettoDeLorean rotation={[0, Math.PI / 2, 0.2]} scale={isMobile ? 0.15 : 0.2} />
+        <directionalLight intensity={10} position={[0, 1, 0]} />
+        <Environment preset='night' />
+        <PerspectiveCamera position={[0, 0, 10]} makeDefault />
+        <OrbitControls
+          autoRotate={isAnimated}
+          autoRotateSpeed={-0.5}
+          enablePan={false}
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+          enableDamping
+        />
+      </Suspense>
     </Canvas>
   )
 }
