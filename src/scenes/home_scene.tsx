7533d704daa6f98b6125289fd2c953b501915ea8
esteban-vm <estebanvm90@outlook.com>
2024-07-22 22:52:22 -0400

    improve loading performance of models

--- src/scenes/home_scene.tsx
+++ src/scenes/home_scene.tsx
@@ -1,29 +1,32 @@
 import { Environment, Html, OrbitControls, PerspectiveCamera } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
-import { Suspense } from 'react'
+import dynamic from 'next/dynamic'
 import { useSceneContext } from '@/contexts'
-import { CyberpunkCity, CyberpunkGhettoDeLorean } from '@/models'
 import { LoadingSpinner } from '@/shared'
+
+const CyberpunkCity = dynamic(() => import('@/models').then((mod) => mod.CyberpunkCity), {
+  loading() {
+    return (
+      <Html center>
+        <LoadingSpinner size={30} />
+      </Html>
+    )
+  },
+})
+
+const CyberpunkGhettoDeLorean = dynamic(() => import('@/models').then((mod) => mod.CyberpunkGhettoDeLorean))
 
 export default function HomeScene() {
   const { isMobile, isAnimated } = useSceneContext()
 
   return (
     <Canvas className='cursor-grab active:cursor-grabbing'>
-      <Suspense
-        fallback={
-          <Html center>
-            <LoadingSpinner size={30} />
-          </Html>
-        }
-      >
-        <CyberpunkCity
-          position={[-2, isMobile ? -2.5 : -2, 2]}
-          rotation={[0, Math.PI / 2, 0]}
-          scale={isMobile ? 1 : 1.2}
-        />
-        <CyberpunkGhettoDeLorean rotation={[0, Math.PI / 2, 0.2]} scale={isMobile ? 0.15 : 0.2} />
-      </Suspense>
+      <CyberpunkCity
+        position={[-2, isMobile ? -2.5 : -2, 2]}
+        rotation={[0, Math.PI / 2, 0]}
+        scale={isMobile ? 1 : 1.2}
+      />
+      <CyberpunkGhettoDeLorean rotation={[0, Math.PI / 2, 0.2]} scale={isMobile ? 0.15 : 0.2} />
       <directionalLight intensity={10} position={[0, 1, 0]} />
       <Environment preset='night' />
       <PerspectiveCamera position={[0, 0, 10]} makeDefault />
