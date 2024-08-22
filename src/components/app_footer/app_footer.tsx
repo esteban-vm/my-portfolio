8ba4a245506755c3b37b613c24e5ce42100a663b
esteban-vm <estebanvm90@outlook.com>
2024-08-21 23:00:04 -0400

    initial trials on music btn

--- src/components/app_footer/app_footer.tsx
+++ src/components/app_footer/app_footer.tsx
@@ -1,14 +1,27 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { TbMusic, TbMusicOff } from 'react-icons/tb'
 import Spain from '@/images/Spain.png'
 import UnitedKingdom from '@/images/UnitedKingdom.png'
 import * as Styled from './app_footer.styled'
 
 export default function AppFooter() {
+  const audioRef = useRef<HTMLAudioElement>(null!)
   const [musicOn, setMusicOn] = useState(false)
 
   const musicOnLbl = 'Music on'
   const musicOffLbl = 'Music off'
+
+  useEffect(() => {
+    audioRef.current.volume = 0.2
+  }, [])
+
+  useEffect(() => {
+    if (musicOn) {
+      audioRef.current.play()
+    } else {
+      audioRef.current.pause()
+    }
+  }, [musicOn])
 
   return (
     <Styled.Wrapper>
@@ -23,6 +36,16 @@ export default function AppFooter() {
         &nbsp;
         <Styled.FlagImage alt='United Kingdom' src={UnitedKingdom} />
       </Styled.InnerWrapper>
+
+      <audio ref={audioRef} loop>
+        <track kind='captions' />
+        {/* 
+          Music from #Uppbeat (free for Creators!):
+          https://uppbeat.io/t/aavirall/cosmic-love
+        */}
+        <source src='/audios/cosmic-love.mp3' type='audio/mpeg' />
+        <code>audio</code> not supported
+      </audio>
     </Styled.Wrapper>
   )
 }
