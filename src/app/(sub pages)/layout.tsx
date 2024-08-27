298ed626fc0feeea3c8b549c9e67ae9e3641b37b
esteban-vm <estebanvm90@outlook.com>
2024-08-26 23:03:16 -0400

    working on section styling

--- src/app/(sub pages)/layout.tsx
+++ src/app/(sub pages)/layout.tsx
@@ -1,5 +1,10 @@
 import type { ReactNode } from 'react'
 
-export default function SubPagesLayout({ children }: { children: ReactNode }) {
-  return <div className='container mx-auto flex h-screen items-start justify-center pt-36 text-white'>{children}</div>
+export default function SubPagesLayout(props: { children: ReactNode }) {
+  return (
+    <div
+      className='container mx-auto flex h-screen items-start justify-center py-[14vh] md:py-[15vh] lg:py-[16vh]'
+      {...props}
+    />
+  )
 }
